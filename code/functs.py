9f40a812456686b936701ed6d7936583c9575e80
ABonvin <89988280+ABonvin@users.noreply.github.com>
2021-12-16 15:37:30 +0100

    commit semi final

--- code/functs.py
+++ code/functs.py
@@ -2,7 +2,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 
-
+#We evalute the classifier, simple
 def evaluate(true, pred):
     precision = precision_score(true, pred, average= 'macro')
     recall = recall_score(true, pred, average = 'macro')
@@ -12,5 +12,17 @@ def evaluate(true, pred):
     print(cf)
     print(f"ACCURACY SCORE:\n{accuracy_score(true, pred):.4f}")
     print(f"CLASSIFICATION REPORT:\n\tPrecision: {precision:.4f}\n\tRecall: {recall:.4f}\n\tF1_Score: {f1:.4f}")
-
-
+    
+    
+#Fucntion to get the scores and add them to a liste of scores for final comparaison
+def get_score(true, pred, classifier_name):
+    
+    precision = precision_score(true, pred, average= 'macro')
+    recall = recall_score(true, pred, average = 'macro')
+    f1 = f1_score(true, pred, average = 'macro')
+    accuracy = accuracy_score(true, pred)
+    cf = confusion_matrix(true, pred)
+    cf = sns.heatmap(cf, annot=True)
+    a, b = [classifier_name, precision, recall, f1, accuracy], cf
+    
+    return a, b
--- code/keras_funct.py
+++ code/keras_funct.py
@@ -24,15 +24,16 @@ def type_counter_unlabelled(df_fit, df_transform):
     nlp = spacy.load('fr_core_news_md')
     dif_typ = []
     
+#Create a list of all they types of words    
     for i in range(df_fit.shape[0]):
         doc = nlp(df_fit['sentence'].iloc[i])
-        
+
         for j in doc:
             if j.pos_ not in dif_typ:
                 dif_typ.append(j.pos_)
                 
     res = []
-    
+ #Increase the counter on each type of word for each sentence    
     for i in range(df_transform.shape[0]):
         temp = np.zeros(len(dif_typ))
         doc = nlp(df_transform['sentence'].iloc[i])
@@ -41,7 +42,7 @@ def type_counter_unlabelled(df_fit, df_transform):
             temp[ind] += 1
         res.append(temp)
         
-                
+    #Return a matrix of all counters           
     return res
 
 
@@ -69,11 +70,13 @@ def type_counter(df):
                 
     return res
 
-
+#Sklearn vecotrizer using a CountVectorizer
 def sk_vec(df):
     phrases = df['sentence'].tolist()
     vectorizer = CountVectorizer()
+    #Also takes Bigrams into account
     vectorizer = CountVectorizer(ngram_range=(1, 2), analyzer='word' )
+    #Return a matrix of vectorized words and BiGrams
     return vectorizer.fit_transform(phrases).toarray()
 
 
@@ -85,7 +88,7 @@ def sk_vec_unlabelled(df_fit, df_transform):
     vectorizer.fit(phrases_fit)
     return vectorizer.transform(phrases_transform).toarray()
 
-
+#Filter of words from the internet, source: https://stackoverflow.com/questions/45375488/how-to-filter-tokens-from-spacy-document
 def remove_tokens_on_match(doc):
     indexes = []
     for index, token in enumerate(doc):
@@ -98,7 +101,7 @@ def remove_tokens_on_match(doc):
     return doc2
 
 
-#This vectorizer is used
+#Spacy vectorizer, converts words to token and then to vectors
 def vectorizer(df):
     tokenized_sentence = []
     nlp = spacy.load('fr_core_news_md')
@@ -107,20 +110,21 @@ def vectorizer(df):
         doc = remove_tokens_on_match(doc)
 
         tokenized_sentence.append(doc.vector)
-        
+    #Return a matrix of vectorized words    
     return tokenized_sentence    
 
-
+#Counter of words length and senteces length based on characters
 def word_len_counter(df):
     res = []
     nlp = spacy.load('fr_core_news_md')
     for sentence in df['sentence'].tolist():
         temp = []
         doc = nlp(sentence)
-        
+        #We create a list for each sentence with both length and we add it to the main list
         temp.append(len(doc))
         temp.append(len(sentence))
         res.append(temp)
+    #Return a matrix of lengths    
     return res
         
     
@@ -132,11 +136,10 @@ def prepare_output(df):
     res2 = []
     mapping =  {'A1':0, 'A2':1, 'B1':2,'B2':3,'C1':4,'C2':5}
     for i in range(len(df)):
+        #We create an empty matrix and populate it with 1 to 1-hot-encode the difficulties
         temp = np.zeros(6)
         temp[mapping[df['difficulty'].iloc[i]]] = 1
         res2.append(temp)
         
-    
+    #We return both y
     return res1, res2
-
-
